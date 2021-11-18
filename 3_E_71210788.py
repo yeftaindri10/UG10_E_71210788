f760a61bcc08abe04e046c718df73aa6f2e8ba43
yefta.indriawati <yefta.indriawati@ti.ukdw.ac.id>
2021-11-19 03:31:37 +0700

    Menambahkan jawaban 3

---
+++ 3_E_71210788.py
@@ -0,0 +1,7 @@
+siswa = input("Masukan nama siswa : ")
+
+print("Daftar Siswa : ", [siswa.title()])
+
+tambah = input("Masukkan siswa yang ingin ditambahkan : ")
+
+print("Hasil penambahan pada daftar siswa : ", [siswa.title(), tambah.upper()])
